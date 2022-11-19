13931a59fbe8a6ec11d744f68a102873d6104157
Jack <jack.morgan.edwards@gmail.com>
2022-11-19 09:56:43 -0500

    twelve tone matrix calc, fixed all twelve tone functions

--- src/TwelveToneSet.ts
+++ src/TwelveToneSet.ts
@@ -1,52 +1,63 @@
 import NoteSet from "./NoteSet";
 
-class TwelveToneSet {
-  series: number[];
+class TwelveToneSet extends NoteSet {
+  set: number[];
 
-  constructor(input_series :number[]){
-    this.series = input_series;
+  constructor(input_series: number[]){
+    super(input_series)
   }
 
   getP0(){
-    let amount = this.series[0];
-    return this.series.map(note => note -= amount);
+    let amount = this.set[0];
+    return NoteSet.normalizeSet(new NoteSet(this.set.map(note => note -= amount)));
   }
 
   getPrime(pitchClass: number){
     let p0 = this.getP0();
-    return p0.map(note => note += pitchClass);
+    return NoteSet.normalizeSet(new NoteSet(p0.set.map(note => note += pitchClass)));
   }
 
   getR0(){
     let p0 = this.getP0();
-    return p0.reverse();
+    return new NoteSet(p0.set.reverse());
   }
 
   getRetrograde(pitchClass: number){
     let retro = this.getPrime(pitchClass);
-    return retro.reverse();
+    return NoteSet.normalizeSet(new NoteSet(retro.set.reverse()));
   }
 
   getI0(){
     let p0 = this.getP0();
-    let note =  NoteSet.invertSet(new NoteSet([...p0]))
-    return note.set;
+    return NoteSet.invertSet(p0);
   }
 
   getInversion(pitchClass: number){
-    let primeSet = this.getPrime(pitchClass);
-    let inverted =  NoteSet.invertSet(new NoteSet([...primeSet]))
-    return inverted.set;
+    //first invert pitchClass, then get prime
+    let primeSet = this.getPrime(12 - pitchClass);
+    return NoteSet.invertSet(primeSet);
   }
 
   getRI0(){
     let I0 = this.getI0();
-    return I0.reverse();
+    return new NoteSet(I0.set.reverse());
   }
 
   getRetrogradeInversion(pitchClass: number){
     let inversion = this.getInversion(pitchClass);
-    return inversion.reverse();
+    return new NoteSet(inversion.set.reverse());
+  }
+
+  getMatrix(){
+    let p0 = this.getP0();
+    let i0 = this.getI0();
+    let matrix = []
+    matrix.push(p0.set);
+    for (let i = 1; i<i0.set.length; i++){
+      let row = this.getPrime(i0.set[i]);
+      matrix.push(row.set)
+    }
+    return matrix;
   }
 
 }
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,7 @@ import PitchClassInterval from "./PitchClassInterval";
 import maps from "./MidiMap";
 import HelperFunctions from "./HelperFunctions";
 import NoteSet from "./NoteSet";
+import TwelveToneSet from "./TwelveToneSet";
 
 let map = maps.pianoKeyToNoteName();
 let map2 = maps.midiNoteToNoteName();
@@ -9,7 +10,7 @@ let map2 = maps.midiNoteToNoteName();
 // console.log(HelperFunctions.normalizePitchClass(25));
 // console.log(HelperFunctions.convertNameToPitchClass("C#"));
 
-let bosh = new PitchClassInterval(7, 8);
+// let bosh = new PitchClassInterval(7, 8);
 // console.log(bosh.getOrderedPitchClassInterval());
 
 // console.log(NoteSet.getNormalForm(new NoteSet([0, 4, 8, 9, 11])))
@@ -24,4 +25,43 @@ let bosh = new PitchClassInterval(7, 8);
 // console.log(NoteSet.checkIfTransposition(new NoteSet([10, 11, 1, 2]), new NoteSet([7, 8, 10, 11])))
 // console.log(NoteSet.checkIfTransposition(new NoteSet([10, 11, 1, 2]), new NoteSet([7, 8, 10, 10])))
 // console.log(NoteSet.invertSet(new NoteSet([1, 3, 4, 7]), 5));
-console.log(NoteSet.getPrimeForm(new NoteSet([1, 5, 6, 7])));
+// console.log(NoteSet.getPrimeForm(new NoteSet([1, 5, 6, 7])));
+let bosh = new TwelveToneSet([2, 1, 9, 10, 5, 3, 4, 0, 8, 7, 6, 11]);
+console.log(bosh.getMatrix())
+// let p0 = bosh.getP0();
+// // console.log(p0.set)
+// let p2 = bosh.getPrime(2)
+// let p7 = bosh.getPrime(7)
+// let p11 = bosh.getPrime(11);
+// // console.log(p2)
+// // console.log(p7)
+// // console.log(p11)
+
+// let r0 = bosh.getR0();
+// // console.log(r0)
+
+// let r2 = bosh.getRetrograde(2)
+// let r7 = bosh.getRetrograde(7)
+// let r11 = bosh.getRetrograde(11);
+
+// // console.log(r2);
+// // console.log(r7)
+// // console.log(r11)
+
+// let I0 = bosh.getI0();
+// // console.log(I0);
+
+// let i2 = bosh.getInversion(2);
+// let i11 = bosh.getInversion(11);
+
+// // console.log(i2)
+// // console.log(i11)
+
+// let ri0 = bosh.getRI0();
+// let ri5 = bosh.getRetrogradeInversion(5);
+// let ri11 = bosh.getRetrogradeInversion(11);
+
+// console.log(ri0)
+
+// console.log(ri5);
+// console.log(ri11);
