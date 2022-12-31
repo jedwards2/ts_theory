93a18cf1716a374277d6c17c67dc5cf8ed50a1e5
Jack <jack.morgan.edwards@gmail.com>
2022-12-31 13:30:20 -0500

    triad constructor

--- src/Triad.ts
+++ src/Triad.ts
@@ -2,8 +2,15 @@ import Chord from "./Chord";
 import { Note } from "./types";
 
 class Triad extends Chord {
+  _root: Note;
+  _third: Note;
+  _fifth: Note;
+
   constructor(inputted_notes: Note[]){
     super(inputted_notes);
+    this._root = inputted_notes[0];
+    this._third = inputted_notes[1];
+    this._fifth = inputted_notes[2];
   };
 }
 
